585e9bae9868e7ba2dabf902dfc2ad574f4b4f16
samuel <it.fischer.samuel@gmail.com>
2021-10-25 23:38:54 +0200

    today's final commit

--- js/lan_switch.js
+++ js/lan_switch.js
@@ -22,7 +22,16 @@ function language_en(){
     document.getElementById("landrop").textContent="EN"
     localStorage.setItem('language', "EN");
 
-    document.getElementById("me_card_text").textContent="I am Samuel Fischer, an italian developer with the passion of learning new things! my "
+    document.getElementById("nav-home").textContent="Home"
+    document.getElementById("nav-about").textContent="About Me"
+    document.getElementById("nav-pictures").textContent="Pictures"
+    document.getElementById("nav-skils").textContent="Skils"
+    document.getElementById("nav-projects").textContent="Projects"
+    document.getElementById("nav-contacts").textContent="Contacts"
+
+    document.getElementById("me_card_text").textContent="I am Samuel Fischer, an italian developer with the passion of learning new things!"
+    document.getElementById("AboutMe").textContent="About Me"
+    document.getElementById("AboutMe_text").textContent="I am a coder from triest that loves web development and taking pictures, in my free tim i play minecraft with my friends on my private server."
     document.getElementById("pictures_id").textContent="Pictures"
     document.getElementById("prog_lan").textContent="Coding skils"
     document.getElementById("project_id").textContent="Projects"
@@ -41,7 +50,16 @@ function language_it(){
     document.getElementById("landrop").textContent="IT"
     localStorage.setItem('language', "IT");
 
+    document.getElementById("nav-home").textContent="Casa"
+    document.getElementById("nav-about").textContent="Di me"
+    document.getElementById("nav-pictures").textContent="Foto"
+    document.getElementById("nav-skils").textContent="Abilità"
+    document.getElementById("nav-projects").textContent="Progetti"
+    document.getElementById("nav-contacts").textContent="Contatti"
+
     document.getElementById("me_card_text").textContent="Sono Samuel Fischer, un developer italiano con la passione di imparare sempre nuove cose!"
+    document.getElementById("AboutMe").textContent="Qualcosa di me"
+    document.getElementById("AboutMe_text").textContent="Sono un programmatore di trieste con la passione per il web development e la fotografia, nel tempo libero adoro fare siti web e giocare a minecraft sul mio server privato dove gioco con dei miei amici."
     document.getElementById("pictures_id").textContent="Fotografie"
     document.getElementById("prog_lan").textContent="Abilità di coding"
     document.getElementById("project_id").textContent="Progetti"
@@ -60,7 +78,16 @@ function language_si(){
     document.getElementById("landrop").textContent="SI"
     localStorage.setItem('language', "SI");
 
+    document.getElementById("nav-home").textContent="Dom"
+    document.getElementById("nav-about").textContent="O meni"
+    document.getElementById("nav-pictures").textContent="Fotografije"
+    document.getElementById("nav-skils").textContent="Sposobnosti"
+    document.getElementById("nav-projects").textContent="Projekti"
+    document.getElementById("nav-contacts").textContent="Kontakti"
+
     document.getElementById("me_card_text").textContent="Jaz sem Samuel Fischer, italjansk coder z voljo se vedno učiti nekaj novega!"
+    document.getElementById("AboutMe").textContent="Nekaj o meni"
+    document.getElementById("AboutMe_text").textContent="Jaz sem en coder iz trsta, ki ljubi izdelati spletne strani in fotografirati, v prostem času pa igram minecraft na moj server z mojmi prijatelji."
     document.getElementById("pictures_id").textContent="Fotografije"
     document.getElementById("prog_lan").textContent="sposobnosti kodiranja"
     document.getElementById("project_id").textContent="Projekti"
