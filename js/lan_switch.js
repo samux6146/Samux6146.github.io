b5b5a130af49542dc320eed95f837b01f2347b47
Samuel Fischer <it.fischer.samuel@gmail.com>
2021-10-25 17:10:29 +0200

    Add files via upload

---
+++ js/lan_switch.js
@@ -0,0 +1,72 @@
+language = localStorage.getItem('language')
+
+document.addEventListener("onload", language_set());
+
+
+function language_set(){
+    console.log("set")
+    if (language === "EN"){
+        language_en()
+    }
+    else if (language === "IT"){
+        language_it()
+    }
+    else if (language === "SI"){
+        language_si()
+    }
+    else{
+        language_en
+    }
+}
+
+function language_en(){
+    document.getElementById("landrop").textContent="EN"
+    localStorage.setItem('language', "EN");
+
+    document.getElementById("me_card_text").textContent="I am Samuel Fischer, an italian developer with the passion of learning new things!"
+    document.getElementById("prog_lan").textContent="en"
+    document.getElementById("project_id").textContent="en"
+}
+
+function language_it(){
+    document.getElementById("landrop").textContent="IT"
+    localStorage.setItem('language', "IT");
+
+    document.getElementById("me_card_text").textContent="Sono Samuel Fischer, un developer italiano con la passione di imparare sempre nuove cose!"
+    document.getElementById("prog_lan").textContent="it"
+    document.getElementById("project_id").textContent="it"
+}
+
+function language_si(){
+    document.getElementById("landrop").textContent="SI"
+    localStorage.setItem('language', "SI");
+
+    document.getElementById("me_card_text").textContent="si"
+    document.getElementById("prog_lan").textContent="si"
+    document.getElementById("project_id").textContent="si"
+}
+
+
+function hover_en(){
+    document.getElementById("en").style.color="black"
+}
+
+function hover_it(){
+    document.getElementById("it").style.color="black"
+}
+
+function hover_si(){
+    document.getElementById("si").style.color="black"
+}
+
+function out_en(){
+    document.getElementById("en").style.color="white"
+}
+
+function out_it(){
+    document.getElementById("it").style.color="white"
+}
+
+function out_si(){
+    document.getElementById("si").style.color="white"
+}
