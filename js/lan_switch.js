8f0e932f1b359047140f7f86779fcb0ec93eed6f
samuel <it.fischer.samuel@gmail.com>
2021-10-25 22:35:10 +0200

    added language things

--- js/lan_switch.js
+++ js/lan_switch.js
@@ -29,11 +29,11 @@ function language_en(){
     Array.prototype.map.call(document.querySelectorAll(".lang_card_but_repo"), function(t) {t.textContent = "Repo on GitHub";});
     document.getElementById("contact").textContent="Contacts"
 
-    document.getElementById("c1_des").textContent="en"
-    document.getElementById("c2_des").textContent="en"
-    document.getElementById("c3_des").textContent="en"
-    document.getElementById("c4_des").textContent="en"
-    document.getElementById("c5_des").textContent="en"
+    document.getElementById("c1_des").textContent="The OpenSource project of this website!"
+    document.getElementById("c2_des").textContent="The project of how i installed mac os 10.13 high sierra on my Lenovo Thinkpad T420."
+    document.getElementById("c3_des").textContent="The project of my 8th grade exam in slovenian."
+    document.getElementById("c4_des").textContent="Croxsplit44 my custom mechanical keyboard with qmk, via and alps switches."
+    document.getElementById("c5_des").textContent="Squorari is a simpla web page made with html and javascript using node.js that enables me, too se the temporary school class calendar for every day in a practical way."
 }
 
 function language_it(){
@@ -47,29 +47,29 @@ function language_it(){
     Array.prototype.map.call(document.querySelectorAll(".lang_card_but_repo"), function(t) {t.textContent = "Repo su GitHub";});
     document.getElementById("contact").textContent="Contatti"
 
-    document.getElementById("c1_des").textContent="it"
-    document.getElementById("c2_des").textContent="it"
-    document.getElementById("c3_des").textContent="it"
-    document.getElementById("c4_des").textContent="it"
-    document.getElementById("c5_des").textContent="it"
+    document.getElementById("c1_des").textContent="Il progetto open source di questo sito!"
+    document.getElementById("c2_des").textContent="Il progetto di come ho installato mac os 10.13 high sierra sul mio Lenovo Thinkpad T420."
+    document.getElementById("c3_des").textContent="Il progetto della mia tesina di terza media sull' internet in lingua slovena."
+    document.getElementById("c4_des").textContent="Croxsplit44 la mia tastiera mechanica OpenSource con qmk, via e tasti alps."
+    document.getElementById("c5_des").textContent="Squorari e una semplice web app che pesca in automatico gli ultimi orari della mia scuola."
 }
 
 function language_si(){
     document.getElementById("landrop").textContent="SI"
     localStorage.setItem('language', "SI");
 
-    document.getElementById("me_card_text").textContent="si"
+    document.getElementById("me_card_text").textContent="Jaz sem Samuel Fischer, italjansk coder z voljo se vedno učiti nekaj novega!"
     document.getElementById("prog_lan").textContent="sposobnosti kodiranja"
     document.getElementById("project_id").textContent="Projekti"
     Array.prototype.map.call(document.querySelectorAll(".lang_card_but_try"), function(t) {t.textContent = "Oglej si stran!";});
     Array.prototype.map.call(document.querySelectorAll(".lang_card_but_repo"), function(t) {t.textContent = "Repo na GitHub";});
     document.getElementById("contact").textContent="Kontakti"
 
-    document.getElementById("c1_des").textContent="si"
-    document.getElementById("c2_des").textContent="si"
-    document.getElementById("c3_des").textContent="si"
-    document.getElementById("c4_des").textContent="si"
-    document.getElementById("c5_des").textContent="si"
+    document.getElementById("c1_des").textContent="Project OpenSource od te spletne strani!"
+    document.getElementById("c2_des").textContent="Projekt o kako sem installiral mac os 10.13 high sierra na moj Lenovo Thinkpad T420."
+    document.getElementById("c3_des").textContent="Projekt od moje končne raziskave 3 srednje o spletu v slovenščini."
+    document.getElementById("c4_des").textContent="Croxsplit44 je moja mehanska timkovnica z qmk, via in alps tipke."
+    document.getElementById("c5_des").textContent="Squorari je ena spletna applikacja za ogledanje urnikov moje šole."
 }
 
 
